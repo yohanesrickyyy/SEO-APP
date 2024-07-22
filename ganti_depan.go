17c56633d970dad9fe517bd004573da377440a55
yohanes <yohanesrickyyy@gmail.com>
2024-07-22 15:06:58 +0700

    feat(spasi, prefix)

---
+++ ganti_depan.go
@@ -0,0 +1,11 @@
+package main
+
+// Daftar kata yang akan diganti awalan dengan =
+var gantidepan = map[string]bool{
+	"ml":    true,
+	"inch": true,
+	"cm":    true,
+	"liter": true,
+	"kg":    true,
+	"harga": true,
+}
--- main.go
+++ main.go
@@ -25,14 +25,40 @@ func rearrangeSentence(input string) string {
 	return input
 }
 
-// Fungsi untuk mengganti spasi dengan karakter tertentu
+// Fungsi untuk mengganti spasi dengan karakter tertentu dalam frasa tertentu
 func replaceSpaces(input, replacement string) string {
 	for phrase := range spasi {
-		if strings.Contains(strings.ToLower(input), phrase) {
-			input = strings.ReplaceAll(input, phrase, strings.ReplaceAll(phrase, " ", replacement))
+		if strings.Contains(strings.ToLower(input), strings.ToLower(phrase)) {
+			modifiedPhrase := strings.ReplaceAll(phrase, " ", replacement)
+			input = strings.ReplaceAll(input, phrase, modifiedPhrase)
 		}
 	}
 	return input
+}
+
+// Fungsi untuk mengganti awalan kata tertentu dengan =
+func replacePrefixes(input string) string {
+	words := strings.Fields(input)
+	var result strings.Builder
+
+	for i := 0; i < len(words); i++ {
+		trimmedWord := words[i]
+
+		if gantidepan[strings.ToLower(trimmedWord)] {
+			// Tambahkan = dan kata yang diubah
+			result.WriteString("=" + trimmedWord)
+		} else {
+			// Jika bukan kata yang diganti, tambahkan kata tersebut ke hasil
+			result.WriteString(trimmedWord)
+		}
+
+		// Tambahkan spasi jika bukan kata terakhir
+		if i < len(words)-1 {
+			result.WriteString(" ")
+		}
+	}
+
+	return result.String()
 }
 
 func main() {
@@ -53,7 +79,8 @@ func main() {
 	for _, sentence := range sentences {
 		modified := rearrangeSentence(sentence)
 		modifiedWithReplacement := replaceSpaces(modified, replacementChar)
-		_, err := writer.WriteString(modifiedWithReplacement + "\n")
+		finalResult := replacePrefixes(modifiedWithReplacement)
+		_, err := writer.WriteString(finalResult + "\n")
 		if err != nil {
 			fmt.Println("Error writing to file:", err)
 			return
